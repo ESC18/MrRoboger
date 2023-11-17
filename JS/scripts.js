25d5b769d55d04dc50e2fce3f2182a5c16482442
ESC18 <ponderosaprintingus@gmail.com>
2023-11-17 15:52:27 -0800

    Add comments

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+//Creates array from 0 to user number.
 function arrayGenerator(input) {
     let newArray = [];
     for (let i = 0; input >= i; i++) {
@@ -6,6 +8,7 @@ function arrayGenerator(input) {
      return textInsertion(newArray);
 }
 
+//Takes array from arrayGenerator and replaces numbers with specified strings if they contain 1,2, or 3.
 function textInsertion(newArray) {
     let resultArray = []
     newArray.forEach(function(number) {
@@ -23,11 +26,14 @@ function textInsertion(newArray) {
     return resultArray;
 }
 
+//UI Logic
+//Calls business logic and returns value to User.
 function displayResult(inputNumber) {
     let result = document.getElementById("result");
     return result.innerHTML = arrayGenerator(inputNumber).join(" ");
 }
 
+//Initializes Code
 function initializer() {
     let form = document.getElementById("form");
     form.addEventListener("submit", function(event) {
